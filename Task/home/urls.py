89dbb4612e0366be1a3ee82c94ede6cfc0c6b891
PranayShriwas <pranayshriwas420@gmail.com>
2023-07-28 19:26:17 +0530

    Commit

--- Task/home/urls.py
+++ Task/home/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .import views
+from .views import UserRegistrationAPIView, UserListAPIView, UserLoginAPIView
 urlpatterns = [
     path('', views.index, name='home'),
     path('login/', views.login, name='login'),
     path('registration/', views.registration, name='registration'),
     path('login_data/', views.login_data, name='login_data'),
-    path('success/', views.success, name='success')
+    path('table/', views.table, name='table'),
+    path('api/register/', UserRegistrationAPIView.as_view(),
+         name='user-registration'),
+    path('api/login/', UserLoginAPIView.as_view(), name='user-login'),
+    path('api/users/', UserListAPIView.as_view(), name='user-list'),
 ]
--- Task/home/views.py
+++ Task/home/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from .models import Registration
 from django.contrib.auth.hashers import make_password, check_password
+from rest_framework.views import APIView
+from .serializers import RegistrationSerializer
+from django.contrib.auth import authenticate, login
+from rest_framework import generics, permissions
+from rest_framework.response import Response
 # Create your views here.
 
 
@@ -41,12 +46,47 @@ def login_data(request):
             user_obj = Registration.objects.get(Email=email)
             user_password = user_obj.Password
             if check_password(password, user_password):
-                return redirect('/success/')
+                print(user_obj.Name)
+                return render(request, 'table.html', {'form': user_obj})
             else:
                 return render(request, 'login.html', {'msg': 'Password Incorrect'})
         else:
             return render(request, 'login.html', {'msg': 'Email Incorrrect'})
 
 
-def success(request):
-    return render(request, 'success.html')
+def table(request):
+    form = Registration.objects.all()
+    return render(request, 'table.html', {'form': form})
+
+# api view for user registration
+
+
+class UserRegistrationAPIView(generics.CreateAPIView):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [permissions.AllowAny]  # Allow any user to register
+
+
+class UserLoginAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        # Get username and password from request data
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Authenticate user
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # Login the user
+            login(request, user)
+            return Response({'message': 'Login successful!', 'user_id': user.id}, status=200)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=401)
+
+# api view for user list (requires authentication)
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [permissions.IsAuthenticated]
